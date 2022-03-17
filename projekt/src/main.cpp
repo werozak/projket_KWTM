046702f0b6aafee0791477f51d2cc99ed88311cf
student <student@Mac-mini.local>
2022-03-17 12:22:21 +0100

    zadanie 3

--- projekt/src/main.cpp
+++ projekt/src/main.cpp
@@ -1,9 +1,24 @@
+#include <LaborkaConfig.h>
 #include <iostream>
 #include <trygonometria.h>
+#ifdef USE_TRIGONOMETRY_DEGREE
+    #include "Trygonometria/trygonometria.h"
+#else
+    #include <cmath>
+#endif
 
 int main() { 
 
 std::cout<<"Hello World!\n"; 
-double zmienna  = degreemath::sin(90.0);
-std::cout<<zmienna<<std::endl;
+//double zmienna  = degreemath::sin(90.0);
+//std::cout<<zmienna<<std::endl;
+
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+     double zmienna  = degreemath::sin(90.0);
+#else
+    double zmienna = sin(M_PI/4.0); 
+#endif
+
+    std::cout<<zmienna<<std::endl;
 }
